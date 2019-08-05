8a0f423ed9f02def344214ee394c6c3e9e3941cc
Konstantin Petrov <kpetrov@ripe.net>
2019-08-05 09:15:22 +0200

    Add checks for input and output

--- src/jrnl-to-vimwiki.py
+++ src/jrnl-to-vimwiki.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
 
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description='Generate vimwiki files out of jrnl.sh journal')
 parser.add_argument('-i','--input', help='Input file name',required=True)
 parser.add_argument('-o','--output',help='Output directory name', required=False)
 args = parser.parse_args()
 
-#TODO Add checks if input file and output dir exist
+if not os.path.exists(args.input):
+  print("Input file doesn't exist!")
+  exit(1)
+
+if not (os.path.exists(args.output) and os.path.isdir(output)):
+  print("Output path either not exist or not a directory!")
+  exit(1)
 
 with open(args.input, "r") as ins:
   array = []
